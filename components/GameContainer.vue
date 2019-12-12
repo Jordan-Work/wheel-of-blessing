4e035cd78d7bdbd8da84b4b5d4367f12bb471b28
Jordan Webb <jordanwebbs@gmail.com>
2019-12-12 02:11:38 -0500

    gameContainer setup

---
+++ components/GameContainer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input id="letter-input" placeholder="Enter a Letter">
+    <p>{{statement}}</p>
+    
+    <Game-Grid :gridWidth="gridWidth"
+      :gridHeight="gridHeight"
+      :message="defaultStatement" />
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import  GameGrid from './GameGrid.Vue'
+
+export default {
+
+  components: {
+    GameGrid
+  },
+  computed: {
+    ...mapState(['defaultStatement']),
+    gridWidth() {
+      return this.$store.state.minimumGridSize;
+    },
+    gridHeight() {
+      // calc the grid rows for the size of the statement 
+      return this.$store.state.defaultStatement.length / 10 + 1;
+    },
+  },
+}
+</script>
