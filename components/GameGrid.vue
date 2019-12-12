018bc086ce4ac643e543ea120ca1d05f0bb10c46
Jordan Webb <jordanwebbs@gmail.com>
2019-12-12 02:12:19 -0500

    gameGrid setup to use props and message

---
+++ components/GameGrid.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    {{gridWidth}}
+    {{gridHeight}}
+    <ul v-for="count in gridItems" :key="count">
+      <li>{{message[count]}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    gridWidth: {
+      type: Number
+    },
+    gridHeight: {
+      type: Number
+    },
+    message: {
+      type: String
+    }
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+
+  computed: {
+    gridItems() {
+      return this.gridWidth * this.gridHeight;
+    }
+  },
+}
+</script>
