4e999442d9e96cfc72473e3d54d3a1d693abf141
Jordan Webb <jordanwebbs@gmail.com>
2019-12-12 02:08:09 -0500

    remove default logo, and adding Vuex store

---
+++ store/index.js
@@ -0,0 +1,12 @@
+export const state = () => ({
+  defaultStatement: 'ALL MY DAYS WERE WRITTEN IN YOUR BOOK BEFORE THEY CAME TO BE',
+  minimumGridSize: 10
+})
+
+export const mutations = {
+  
+}
+
+export const getters = {
+  
+}
